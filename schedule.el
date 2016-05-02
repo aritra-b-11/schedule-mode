5ce5e2efc7a73b3f11f7f6734a2681c5f02da49c
Aritra-b-11 <aritrabhattacharjee12@gmail.com>
2016-05-03 00:13:45 +0530

    commit 1

--- schedule.el
+++ schedule.el
@@ -1,12 +1,19 @@
-;; schedule.el --- make scheduling in emacs
+;; schedule.el --- make scheduling in emacs org mode
 
-(defun schedule ()
+(defun schedule (file-name)
   "make schedule"
-(interactive)
-(switch-to-buffer "schedule")
-(org-based-schedule-mode)
-(schedule-init)
-)
+  (interactive "sOpen/Create file: ")
+  (message "File name: %s" file-name)
+  (if (file-exists-p file-name)
+      nil
+    (progn (create-file-buffer file-name)
+     (write-file file-name)
+     (save-buffer)
+     (message "no")))
+  (switch-to-buffer file-name)
+  (org-based-schedule-mode)
+  (schedule-init))
+
 (define-derived-mode org-based-schedule-mode org-mode "org-based-scheduling-mode")
 
 
